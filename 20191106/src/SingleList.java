b5b79399952b6ff87825b33103d84dd65fab60f4
sabrina-LYY <930633839@qq.com>
2019-11-07 20:17:56 +0800

    练习

--- 20191106/src/SingleList.java
+++ 20191106/src/SingleList.java
@@ -12,6 +12,48 @@ public class SingleList {
     public SingleList(){
         this.head = null;
     }
+
+    //头插法
+    public void addFirst(int data){
+        ListNode node = new ListNode(data);
+        if(this.head == null){
+            this.head = node;
+        }
+        else{
+            node.next = this.head;
+            this.head = node;
+        }
+
+    }
+
+    //任意位置插入,第一个数据节点为0号下标
+    //1.先找到index-1的位置
+    public ListNode SearchIndex(int index){
+        ListNode prev = this.head;
+        int count = 0;
+        while(count<index-1){
+            prev = prev.next;
+            count++;
+        }
+        return prev;
+
+    }
+    //2.进行插入
+   public boolean addIndex(int index,int data) {
+        if(index<0||index>getLength()){
+            return false;
+        }
+        if(index == 0){
+            addFirst(data);
+            return true;
+        }
+        ListNode prev = SearchIndex(index);
+        ListNode node = new ListNode(data);
+        node.next = prev.next;
+        prev.next = node;
+        return true;
+   }
+
     //反转单链表
     public ListNode reverseList() {
         ListNode prev = null;
@@ -29,7 +71,8 @@ public class SingleList {
         return newHead;
     }
 
-
+    //给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+    // 如果有两个中间结点，则返回第二个中间结点
     public ListNode middleNode() {
         /*int len = getLength()/2;
         ListNode cur = this.head;
@@ -56,6 +99,8 @@ public class SingleList {
         }
         return count;
     }
+
+    //输入一个链表，输出该链表中倒数第k个结点
     public ListNode findKthToTail1(int k) {
         if(k <= 0 ||k > getLength()) {
             return null;
@@ -82,11 +127,11 @@ public class SingleList {
             if(fast.next != null) {
                 fast = fast.next;
                 k--;
-            }else {
+            }
+            else {
                 System.out.println("没有这个节点");
                 return null;
             }
-
         }
         while (fast.next != null) {
             fast = fast.next;
@@ -95,7 +140,7 @@ public class SingleList {
         return slow;
     }
 
-    public void display(ListNode newHead){
+    public void display2(ListNode newHead){
         if(newHead == null) {
             return;
         }
@@ -105,6 +150,18 @@ public class SingleList {
             cur = cur.next;
         }
         System.out.println();
+    }
+
+    public void display(){
+        if(this.head==null){
+            return;
+        }
+        ListNode cur = this.head;
+        while(cur!=null){
+            System.out.print(cur.data+" ");
+            cur = cur.next;
+        }
+        System.out.println("");
     }
 
 }
--- 20191106/src/TestDemo.java
+++ 20191106/src/TestDemo.java
@@ -1,6 +1,25 @@
 public class TestDemo {
     public static void main(String[] args) {
         SingleList singleList = new SingleList();
+        singleList.addFirst(1);
+        singleList.addFirst(3);
+        singleList.addFirst(12);
+        singleList.addFirst(3);
+        singleList.addFirst(5);
+        singleList.display();
+        singleList.addIndex(0,99);
+        singleList.display();
+        singleList.addIndex(3,9);
+        singleList.display();
 
+        ListNode node1 = singleList.reverseList();
+        singleList.display2(node1);
+
+        ListNode node3 =singleList.middleNode();
+        singleList.display2(node3);
+
+
+        ListNode node2 = singleList.findKthToTail(2);
+        singleList.display2(node2);
     }
 }
