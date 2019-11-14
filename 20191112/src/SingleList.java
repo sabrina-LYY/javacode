37671234bf34294550269b09d3b9a26d59fd119a
sabrina-LYY <930633839@qq.com>
2019-11-14 22:23:11 +0800

    提交代码

--- 20191112/src/SingleList.java
+++ 20191112/src/SingleList.java
@@ -38,7 +38,7 @@ class SingleList {
         }
     }
 
-    //计算结点长度
+    //计算链表长度
     public int getLength(){
         int count = 0;
         ListNode  cur = this.head;
@@ -95,47 +95,150 @@ class SingleList {
 
     //找到倒数第k个结点
     public ListNode findKthToTail(int k){
-        ListNode fast = this.head;
-        ListNode slow = this.head;
-
-        if(k<=0){
-            return null;
-        }
-        while(k-1>0){
-            if(fast!=null){
-                fast = fast.next;
-                k--;
-            }
-            else{
-                return null;
-            }
-        }
-        while(fast.next!=null){
-            fast = fast.next;
-            slow = slow.next;
-        }
-        return slow;
+       ListNode fast = this.head;
+       ListNode slow = this.head;
+       if(k<=0){
+           return null;
+       }
+       while(k-1>0){
+           fast = fast.next;
+           k--;
+       }
+       while(fast.next!=null){
+           fast = fast.next;
+           slow = slow.next;
+       }
+       return slow;
     }
 
+
+    //反转链表
     public ListNode reverseList(){
-        ListNode prev=null;
-        ListNode cur = this.head;
-        ListNode newHead = null;
-        if(this.head==null){
-            return null;
-        }
-        while(cur!=null){
-            ListNode curNext = cur.next;
-            if(curNext==null){
-                newHead = cur;
+      ListNode newHead = null;
+      ListNode prev = null;
+      ListNode cur = this.head;
+      while(cur!=null){
+          ListNode curNext = cur.next;
+          if(cur.next == null){
+              newHead = cur;
+          }
+          cur.next = prev;
+          prev = cur;
+          cur = curNext;
+      }
+      return newHead;
+    }
+
+    //删除第一次出现关键字为key的节点
+
+    public ListNode searchPrev(int key){
+        ListNode prev = this.head;
+        while(prev.next!=null){
+            if(prev.next.data==key){
+                return prev;
             }
-            cur.next = prev;
+            prev = prev.next;
+        }
+        return null;
+    }
+    public void remove(int key){
+       if(this.head==null){
+           return;
+       }
+       ListNode prev = searchPrev(key);
+       if(prev==null){
+           System.out.println("没有这个结点");
+           return;
+       }
+       ListNode del = prev.next;
+       prev.next = del.next;
+    }
+
+    //删除所有值为key的节点
+    public void removeAllKey(int key){
+        ListNode prev = this.head;
+        ListNode cur = this.head.next;
+        while(cur!=null){
+            if(cur.data==key){
+                prev.next = cur.next;
+                cur = cur.next;
+            }
             prev = cur;
             cur = cur.next;
-
         }
-        return newHead;
+        if(this.head.data ==key){
+            this.head = this.head.next;
+        }
     }
+
+    //6. 编写代码，以给定值x为基准将链表分割成两部分，
+    // 所有小于x的结点排在大于或等于x的结点之前
+    public ListNode  partition(int x) {
+        ListNode bs = null;
+        ListNode be = null;
+        ListNode as = null;
+        ListNode ae = null;
+        ListNode cur = this.head;
+        if(this.head==null||this.head.next==null){
+            return this.head;
+        }
+        while(cur != null){
+            if(cur.data < x){
+                if(bs == null){
+                    bs = cur;
+                    be = cur;
+                }else{
+                    be.next = cur;
+                    be = be.next;
+                }
+            }else{
+                if(as == null){
+                    as = cur;
+                    ae = cur;
+                }else{
+                    ae.next = cur;
+                    ae = ae.next;
+                }
+            }
+            cur = cur.next;
+        }
+        if(bs == null){
+            return as;
+        }
+        be.next = as;
+        if(as!=null){
+            ae.next = null;
+        }
+        return bs;
+    }
+
+
+    //7. 在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，
+    // 重复的结点不保留，返回链表头指针。
+    public ListNode deleteDuplication(){
+        ListNode cur = this.head;
+        ListNode newHead = new ListNode(-1);
+        ListNode tmp = newHead;
+        while(cur!=null){
+            if(cur.next!=null &&cur.data == cur.next.data){
+                while(cur.next!=null && cur.data == cur.next.data){
+                    cur = cur.next;
+                }
+                cur = cur.next;
+            }else{
+                tmp.next = cur;
+                tmp = cur;
+                cur = cur.next;
+            }
+        }
+        tmp.next = null;
+        return tmp;
+    }
+
+    //8. 链表的回文结构。
+
+
+
 
 
     public void display(){
@@ -147,8 +250,18 @@ class SingleList {
             System.out.print(cur.data+" ");
             cur = cur.next;
         }
-        System.out.println(" ");
+        System.out.println();
     }
 
-
+    public void display1(ListNode newHead){
+        if(newHead==null){
+            return;
+        }
+        ListNode cur = newHead;
+        while(cur!=null){
+            System.out.print(cur.data +" ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
 }
--- 20191112/src/TestDemo.java
+++ 20191112/src/TestDemo.java
@@ -2,16 +2,29 @@ public class TestDemo {
     public static void main(String[] args) {
         SingleList singleList = new SingleList();
         singleList.addFirst(3);
-        singleList.addFirst(4);
+        singleList.addFirst(8);
         singleList.addFirst(5);
         singleList.addFirst(6);
         singleList.addFirst(7);
         singleList.addLast(2);
         singleList.display();
 
+        ListNode node = singleList.partition(5);
+        singleList.display1(node);
+
+        /*singleList.removeAllKey(6);
+        singleList.display();
+
+        singleList.remove(5);
+        singleList.display();
+
+        ListNode newHead = singleList.reverseList();
+        singleList.display1(newHead);
         System.out.println(singleList.addIndex(2,8));
         singleList.display();
 
         System.out.println(singleList.contains(10));
+        ListNode node = singleList.findKthToTail(3);
+        System.out.println(node.data);*/
     }
 }
