b994a3165b004863b878b36ff76592e190e06bba
sabrina-LYY <930633839@qq.com>
2019-11-21 22:24:31 +0800

    练习

--- 20191119/src/TestDemo1.java
+++ 20191119/src/TestDemo1.java
@@ -3,46 +3,40 @@ class Animal{
     public Animal(String name){
         this.name = name;
     }
-    public void eat(){
 
-        System.out.println(this.name+"正在吃");
+    public void eat(String food){
+        System.out.println(this.name+"正在吃"+food);
     }
 }
 class Cat extends Animal{
-    public String sex;
-    public Cat(String name,String sex){
+    public Cat(String name){
         super(name);
-        this.sex = sex;
-    }
-    public void jump(){
-
-        System.out.println(this.name+"jump");
     }
 }
 
 class Bird extends Animal{
-    public String sex;
-    public Bird(String name,String sex){
+    public Bird(String name){
         super(name);
-        this.sex = sex;
     }
+
     public void fly(){
-
-        System.out.println(this.name+"fly");
+        System.out.println(this.name+"正在飞");
     }
-
-
 }
-
 public class TestDemo1 {
     public static void main(String[] args) {
-        //向上转型
+
+        Cat cat = new Cat("小花");
+        cat.eat("猫粮");
+        Bird bird = new Bird("小明");
+        bird.fly();
+        /*//向上转型
         Animal animal = new Cat("小花","nan");
         animal.eat();
         //向下转型
-        Animal animal2 = new Bird("小米","nan");
+        Animal animal2 = new Bird("小明","nan");
         animal2.eat();
         Bird bird =(Bird)animal2;
-        bird.fly();
+        bird.fly();*/
     }
 }
