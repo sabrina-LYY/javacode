0cac7a34dc908444995a617597a2e5846e7663d8
sabrina-LYY <930633839@qq.com>
2020-03-11 21:29:04 +0800

    锁的使用

---
+++ 20200311/src/synchronized用法.java
@@ -0,0 +1,15 @@
+public class synchronized用法 {
+    synchronized void 普通方法(){
+
+    }
+    synchronized static void 静态方法(){
+
+    }
+
+    void 其他方法(){
+        Object 一个引用 = new Object();
+        synchronized (一个引用){
+
+        }
+    }
+}
