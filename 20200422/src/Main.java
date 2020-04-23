8c89373e78a852148b9ec9bc8d7e92c0681e020f
sabrina-LYY <930633839@qq.com>
2020-04-23 11:09:01 +0800

    练习

--- 20200422/src/Main.java
+++ 20200422/src/Main.java
@@ -5,18 +5,48 @@ public class Main {
     //查找兄弟单词
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
         while (in.hasNext()) {
+            List<String> list = new ArrayList<>();
             int n = in.nextInt();
             String[] dict = new String[n];
-            for(int i=0;i<dict.length;i++){
+            for(int i=0;i<n;i++){
                 dict[i] = in.next();
             }
-            String src = in.nextLine();
+            String src = in.next();
             int brother = in.nextInt();
-            
+            Arrays.sort(dict);
+            for(String str:dict){
+                if(isbrother(src,str)){
+                    list.add(str);
+                }
+            }
+            System.out.println(list.size());
+            if(brother<=list.size() && list.size()>0){
+                System.out.println(list.get(brother-1));
+            }
         }
     }
 
+
+    public static boolean isbrother(String src,String str){
+        if(src.equals(str) ) {
+            return false;
+        }
+        if(src.length()!=str.length()){
+            return false;
+        }
+        char[] target = src.toCharArray();
+        char[] strArray = str.toCharArray();
+        Arrays.sort(target);
+        Arrays.sort(strArray);
+        for(int i=0;i<target.length;i++){
+            if(target[i]!=strArray[i]){
+                return false;
+            }
+        }
+        return true;
+    }
 
     //乒乓球筐  牛客
     public static void main1(String[] args) {
